25d08aab4551698c397899dd7557c3feb1b3b86e
Antonio Alves <antoniodecastroalves@gmail.com>
2021-07-28 15:38:35 -0300

    Inicio do desenvolvimento do conteúdo lateral - Parte 02

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Home = () => <div>Heloo world!</div>
+import { Container } from './styles'
+
+const Home = () => <Container>Hello world!</Container>;
 
 export default Home;
