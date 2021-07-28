e662e24787c8024eec1f2003b1fd87913a5a3e2c
Antonio Alves <antoniodecastroalves@gmail.com>
2021-07-28 17:01:51 -0300

    Inicio do desenvolvimento do conteúdo lateral - Parte 02

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
 
-import { Container } from './styles'
+import logo from '../../assets/logo.svg';
 
-const Home = () => <Container>Hello world!</Container>;
+import { Container, Search } from './styles'
 
+const Home = () => {
+  const [inputValue, setInputValue] = useState();
+
+
+  return(
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante"/>
+        <TextField
+          label="Pesquisar"
+          outlined
+          //onTrailingIconSelect={() => this.setState({ value: '' })}
+          //trailingIcon={<MaterialIcon role="button" icon="delete" />}
+          >
+          <Input
+            value={inputValue} onChange={(e) => setInputValue(e.target.value)}/>
+        </TextField>
+      </Search>
+    </Container>
+  );
+};
 export default Home;
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,3 +6,12 @@ export const Container = styled.aside`
   height: 100vh;
   overflow-y: auto;
 `;
+
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 16px;
+
+`;
