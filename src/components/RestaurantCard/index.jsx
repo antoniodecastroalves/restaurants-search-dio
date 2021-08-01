6e4d23972fa45a8583d565baa6d461edca45c947
Antonio Alves <antoniodecastroalves@gmail.com>
2021-08-01 00:03:56 -0300

    desenvolvimento de componentes - Parte 03

--- src/components/RestaurantCard/index.jsx
+++ src/components/RestaurantCard/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { Restaurant, RestaurantInfo, Title, Address } from './styles';
+import ReactStars from 'react-rating-stars-component';
+
+import restaurante from '../../assets/restaurante-fake.png';
+
+import { Restaurant, RestaurantInfo, Title, Address, RestaurantPhoto } from './styles';
 
 const RestaurantCard = () => <Restaurant>
     <RestaurantInfo>
       <Title>Nome do Restaurante</Title>
-      <p>Avaliação</p>
+      <ReactStars count={5} isHalf value={4} edit={false} activeColor="#e7711c"/>
       <Address>Rua Rio de Janeiro, 120</Address>
     </RestaurantInfo>
+    <RestaurantPhoto src={restaurante} alt="Foto do Restaurante" />
 </Restaurant>
 
 export default RestaurantCard;
--- src/components/RestaurantCard/styles.js
+++ src/components/RestaurantCard/styles.js
@@ -34,4 +34,12 @@ export const Address = styled.span`
   font-size: 16px;
   line-height: 19px;
   margin-bottom: 10px;
+  margin-top: 10px;
+`;
+
+export const RestaurantPhoto = styled.img`
+  width: 100px;
+  height: 100px;
+  object-fit: cover;
+  border-radius: 16px;
 `;
