3f963f0c71e6d0e795a13e9aa5046fdf76e479a1
Antonio Alves <antoniodecastroalves@gmail.com>
2021-08-01 00:43:27 -0300

    desenvolvimento de componentes - Parte 04

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Portal from './Portal';
+
+const Modal = () => {
+  return (
+    <Portal>
+      <div>fgfgfgf</div>
+    </Portal>
+  );
+};
+
+export default Modal;
